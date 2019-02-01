ba2fba2ef90420ad5cbc5b425d3bdf1e2947b10d
hyperfraise <damien.menigaux@polytechnique.edu>
2019-02-01 16:50:34 +0100

    save realimages too

--- trainer.py
+++ trainer.py
@@ -426,20 +426,51 @@ class trainer:
                 if self.globalIter % self.config.save_img_every == 0:
                     with torch.no_grad():
                         x_test = self.G(self.z_test)
-                    utils.mkdir("repo/save/grid")
+                    utils.mkdir(self.loader.dir_path + "repo/save/grid")
+                    utils.mkdir(self.loader.dir_path + "repo/save/grid_real")
                     utils.save_image_grid(
                         x_test.data,
-                        "repo/save/grid/{}_{}_G{}_D{}.jpg".format(
+                        self.loader.dir_path
+                        + "repo/save/grid/{}_{}_G{}_D{}.jpg".format(
                             int(self.globalIter / self.config.save_img_every),
                             self.phase,
                             self.complete["gen"],
                             self.complete["dis"],
                         ),
                     )
-                    utils.mkdir("repo/save/resl_{}".format(int(floor(self.resl))))
+                    utils.save_image_grid(
+                        self.x.data,
+                        self.loader.dir_path
+                        + "repo/save/grid_real/{}_{}_G{}_D{}.jpg".format(
+                            int(self.globalIter / self.config.save_img_every),
+                            self.phase,
+                            self.complete["gen"],
+                            self.complete["dis"],
+                        ),
+                    )
+                    utils.mkdir(
+                        self.loader.dir_path
+                        + "repo/save/resl_{}".format(int(floor(self.resl)))
+                    )
+                    utils.mkdir(
+                        self.loader.dir_path
+                        + "repo/save/resl_{}_real".format(int(floor(self.resl)))
+                    )
                     utils.save_image_single(
                         x_test.data,
-                        "repo/save/resl_{}/{}_{}_G{}_D{}.jpg".format(
+                        self.loader.dir_path
+                        + "repo/save/resl_{}/{}_{}_G{}_D{}.jpg".format(
+                            int(floor(self.resl)),
+                            int(self.globalIter / self.config.save_img_every),
+                            self.phase,
+                            self.complete["gen"],
+                            self.complete["dis"],
+                        ),
+                    )
+                    utils.save_image_single(
+                        self.x.data,
+                        self.loader.dir_path
+                        + "repo/save/resl_{}_real/{}_{}_G{}_D{}.jpg".format(
                             int(floor(self.resl)),
                             int(self.globalIter / self.config.save_img_every),
                             self.phase,
